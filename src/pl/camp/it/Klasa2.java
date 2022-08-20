2145a94a0c94efc8ef8879e4f7d1ed972af10a9c
Michał <michalklusek01@gmail.com>
2022-08-20 10:28:22 +0200

    commit2

---
+++ src/pl/camp/it/Klasa2.java
@@ -0,0 +1,13 @@
+package pl.camp.it;
+
+public class Klasa2 {
+    private int a;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+}
